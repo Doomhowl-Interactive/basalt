b9e5490c567f0a223841e23a866efb797d9e95e3
bramtechs <bramtech@telenet.be>
2023-03-14 10:02:40 +0100

    fixed gfx artifact

--- src/basalt_extra_graphics.c
+++ src/basalt_extra_graphics.c
@@ -1,12 +1,13 @@
 #include "basalt.h"
 #include "basalt_extra.h"
 
+// TODO: bounds check frame index
 void BlitTextureSheet(Texture canvas, TextureSheet sheet, int frame, Vec2 pos) {
     Assert(sheet.texture);
     Assert(sheet.texture->pixels);
     Rect source = {
         sheet.origin.x + frame * sheet.cellSize.x,
-        sheet.origin.y + frame * sheet.cellSize.y,
+        sheet.origin.y,
         sheet.cellSize.x,
         sheet.cellSize.y,
     };
