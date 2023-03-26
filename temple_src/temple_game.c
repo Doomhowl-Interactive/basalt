5311f942f37c9e7303859aa5b7df08d97113521f
bramtechs <bramtech@telenet.be>
2023-03-26 18:37:12 +0200

    draw fox sheet

--- temple_src/temple_game.c
+++ temple_src/temple_game.c
@@ -1,6 +1,19 @@
 // compile raylib first: https://github.com/raysan5/raylib/wiki/Working-on-GNU-Linux
 #include "raylib.h"
 
+typedef unsigned int uint;
+typedef Vector2 Vec2;
+typedef Vector3 Vec3;
+
+static char* SPR_RGBA = "assets/spr_rgba.png";
+static char* SPR_PLAYER_FOX = "assets/spr_player_fox.png";
+static char* SPR_BLOCK = "assets/spr_block.png";
+static char* TILE_BLOCK_SMALL = "assets/tile_block_small.png";
+
+static Texture foxSprite;
+
+#include "temple_assets.c"
+
 int main(void)
 {
     // Initialization
@@ -12,6 +25,8 @@ int main(void)
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
+
+    foxSprite = LoadTexture(SPR_PLAYER_FOX);
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -25,9 +40,9 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(SKYBLUE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawTexture(foxSprite, 10, 10, WHITE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
