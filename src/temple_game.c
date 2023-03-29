dc899d90349052e7e4941170b8f25d928c8e92ed
bramtechs <bramtech@telenet.be>
2023-03-29 18:35:31 +0200

    Use delta timing for test fox animation

--- src/temple_game.c
+++ src/temple_game.c
@@ -6,7 +6,7 @@ const uint WIDTH = 640;
 const uint HEIGHT = 480;
 const char* GAME_TITLE = "Basalt Engine";
 
-const uint MAX_FPS = 60;
+const uint MAX_FPS = 180;
 const uint TPS = 25;
 const uint MAX_ENTITIES = 256;
 
@@ -33,25 +33,32 @@ pubfunc void DisposeGame() {
 
 pubfunc void UpdateAndRenderGame(Texture canvas, float delta) {
 
-    static const TextureSheet *anims[] = {
+    static const TextureSheet* anims[] = {
         &ANIM_FOX_IDLE, &ANIM_FOX_WALK, &ANIM_FOX_STOP,
-        &ANIM_FOX_JUMP, &ANIM_FOX_LAND, NULL,
+        &ANIM_FOX_JUMP, &ANIM_FOX_LAND
     };
+    static float timers[5];
+    static int indices[5];
 
     // ClearTexture(canvas, SKY_COLOR);
     DrawWeirdTestGradient(canvas);
 
     // draw each fox animation for testing
-    static int foxFrame = 0;
     int offsetX = 10;
-    for (int i = 0; anims[i] != NULL; i++){
-        TextureSheet sheet = *anims[i];
+    for (int i = 0; i < 5; i++){
+        const TextureSheet sheet = *anims[i];
         assert(sheet.texture);
+
         Vec2 pos = {offsetX, 10};
-        DrawTextureSheet(canvas, sheet, foxFrame, pos);
+        DrawTextureSheet(canvas, sheet, indices[i], pos);
         offsetX += 50;
+
+        timers[i] += delta;
+        if (timers[i] > 1.f / sheet.fps){
+            timers[i] = 0.f;
+            indices[i] ++;
+        }
     }
-    foxFrame++;
 
     // TODO: use raylib's color constants
     DrawRectangle(canvas, 50+00, 250+00, 150, 150, RGB(255,0,0));
