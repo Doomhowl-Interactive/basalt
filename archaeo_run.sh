a60d6da54ca071eb51d0fddd26c0444337769e35
bramtechs <bramtech@telenet.be>
2023-03-31 17:51:17 +0200

    don't build tooling seperately

--- archaeo_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -xe
-
-./tooling_build.sh
--- tooling_build.sh
+++ tooling_build.sh
@@ -9,13 +9,4 @@ cd build
 gcc -ggdb ../src/tooling/embedder.c -o embedder
 ./embedder ../assets ../src/assets_custom.dat.c
 
-# build archaeo
-gcc -ggdb -Werror -DBASALT_DEBUG ../src/basalt_*.c ../src/xorg_basalt.c ../src/tooling/archaeo.c \
-    -lX11 -lm -lXext -I ../src -o archaeo
-
-# build editor
-gcc -ggdb -Werror -DBASALT_DEBUG ../src/basalt_*.c \
-    ../src/tooling/editor.c ../src/xorg_basalt.c ../src/assets_custom.dat.c \
-    -lX11 -lm -lXext -o editor_linux.x11 -I ../src
-
 cd ..
