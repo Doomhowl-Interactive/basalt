40dd4be72d73b51144d26cbf4a3caf8341a3095e
bramtechs <bramtech@telenet.be>
2023-07-18 14:34:18 +0200

    drag profiler panel around

--- basalt/basalt_profiler_gui.cpp
+++ basalt/basalt_profiler_gui.cpp
@@ -1,7 +1,37 @@
+#include <optional>
+
+#include "basalt_macros.hpp"
 #include "basalt_profiler.hpp"
 #include "basalt_graphics.hpp"
+#include "basalt_input.hpp"
 
 using namespace std;
+
+static Rect ProfilerBounds = { 10, 10, 300, 300 };
+
+struct DragContext {
+    Point beginMouse;
+    Point beginPos;
+};
+
+static Rect DragAndDropProfiler(Rect& rect)
+{
+    static optional<DragContext> ctx = nullopt;
+
+    auto mouse = GetMousePosition();
+    if (ctx) {
+        auto mouseOffset = mouse - ctx.value().beginMouse;
+        auto newPos = ctx.value().beginPos + mouseOffset;
+        rect.x = newPos.x;
+        rect.y = newPos.y;
+        if (IsMouseUp()) {
+            ctx = nullopt;
+        }
+    } else if (IsMouseDown() && mouse.inside(rect)) {
+        ctx = { mouse, { rect.x, rect.y } };
+    }
+    return rect;
+}
 
 void DrawProfiler(Texture canvas)
 {
@@ -10,14 +40,16 @@ void DrawProfiler(Texture canvas)
     constexpr int PADDING = 10;
     constexpr int FONT_SIZE = 16;
 
-    static Color bgColor = ColorAlpha(DARKGRAY, 0.8f);
-    static Rect bounds = { 10, 10, 300, 300 };
-    static Rect contentBounds = {
-        bounds.x + PADDING, bounds.y + PADDING, bounds.x + PADDING * 2, bounds.y + PADDING * 2
-    };
+    static Color bgColor = ColorAlpha(DARKGRAY, 0.5f);
+    static Rect contentBounds = { ProfilerBounds.x + PADDING,
+                                  ProfilerBounds.y + PADDING,
+                                  ProfilerBounds.x + PADDING * 2,
+                                  ProfilerBounds.y + PADDING * 2 };
+
+    DragAndDropProfiler(ProfilerBounds);
 
     string text = "Frame: " + to_string(data.frameIndex);
     text += "\nFPS: " + to_string((int)data.fps.average());
     canvas.DrawText(text, contentBounds.x, contentBounds.y, YELLOW);
-    canvas.DrawRectangle(R2(bounds), bgColor);
+    canvas.DrawRectangle(R2(ProfilerBounds), bgColor);
 }
