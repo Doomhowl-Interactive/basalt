1c3ad9e33787b9f6a9226c8d83201e0b0c682176
bramtechs <bramtech@telenet.be>
2023-07-18 21:20:16 +0200

    Packaging

---
+++ launcher/Package.ps1
@@ -0,0 +1,19 @@
+﻿$BasePath = "$PSScriptRoot/.."
+$ReleasePath = "$BasePath/x64/Release"
+$DistributionPath = "$BasePath/Distribution"
+
+# remove old distribution if exists
+if (Test-Path $DistributionPath) {
+    Remove-Item -Recurse -Force $DistributionPath
+}
+New-Item -ItemType Directory -Force -Path $DistributionPath
+
+# copy files to distribution
+Copy-Item -Path "$ReleasePath/*.exe" -Destination $DistributionPath
+Copy-Item -Path "$ReleasePath/*.dll" -Destination $DistributionPath
+Copy-Item -Path "$ReleasePath/assets" -Destination $DistributionPath -Recurse
+
+Write-Host "Distribution created at $DistributionPath"
+
+# Zip distribution to Distribution.zip
+# Compress-Archive -Path $DistributionPath/* -DestinationPath "$BasePath/Distribution.zip" -Force
