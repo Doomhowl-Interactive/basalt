78592264b0e87b93ab8f3656ef029ead756be9af
bramtechs <bramtech@telenet.be>
2023-07-19 19:20:26 +0200

    Basalt3D project

---
+++ basalt_3d/basalt_3d.cpp
@@ -0,0 +1,10 @@
+// basalt_3d.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnbasalt3d()
+{
+}
