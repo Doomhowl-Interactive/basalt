59c89394be3466ff22482948bf5a4e9a541f92a4
bramtechs <bramtech@telenet.be>
2023-08-03 20:01:22 +0200

    tail orientation mostly fixed

--- basalt/basalt_instance.cpp
+++ basalt/basalt_instance.cpp
@@ -5,16 +5,20 @@ using namespace std;
 
 int _RunEntryPoint(int argc, char** argv, function<int(int, char**)> startFunc)
 {
+#if 0
     try {
         return startFunc(argc, argv);
     } catch (exception e) {
         _HandleFatalException(e);
         return -1;
     }
+#else
+    return startFunc(argc, argv);
+#endif
 }
 
 void _HandleFatalException(std::exception e)
 {
     spdlog::error("Fatal exception: {}", e.what());
-    exit(EXIT_FAILURE);
+    throw e;
 }
